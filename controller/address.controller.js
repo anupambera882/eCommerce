8b325a6d9ff19f04eec076a137f2026f3baa65e9
Anupam Bera <anupambera882@gmail.com>
2023-06-24 00:12:41 +0530

    v1

--- controller/address.controller.js
+++ controller/address.controller.js
@@ -1,7 +1,109 @@
 const AddressService = require('../service/address.service');
-
+const UserService = require('../service/user.service');
+const validateMongodbId = require('../utils/validateMongodbId.utils');
 class AddressController {
+    static createAddress = async (req, res) => {
+        try {
+            const { name, mobile, pinCode, address } = req.body;
+            const { userId } = req.user;
+            const newAddress = await AddressService.createNewAddress({
+                name: name,
+                mobile: mobile,
+                pinCode: pinCode,
+                address: address,
+                userId: userId
+            });
+            await UserService.updateUserDetailsById(userId, { $push: { address: newAddress._id } })
+            return res.status(201).json({
+                success: true,
+                message: 'New Address  created successfully',
+                Address: newAddress
+            });
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "Unable to created new Address ",
+                errMessage: err.message
+            });
+        }
+    }
 
+    static updateAddress = async (req, res) => {
+        try {
+            const { id } = req.params;
+            validateMongodbId(id, res);
+            const update = await AddressService.updateAddressDetailsById(id, req.body);
+
+            return res.status(201).json({
+                success: true,
+                message: 'successfully update Address',
+                data: update
+            });
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "unable to update Address",
+                errMessage: err.message
+            });
+        }
+    }
+
+    static deleteAddress = async (req, res) => {
+        try {
+            const { id } = req.params;
+            validateMongodbId(id, res);
+            await AddressService.updateAddressDetailsById(id, { isDeleted: true });
+
+            return res.status(201).json({
+                success: true,
+                message: 'successfully delete Address'
+            });
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "unable to delete Address",
+                errMessage: err.message
+            });
+        }
+    }
+
+    static getAddressById = async (req, res) => {
+        try {
+            const { id } = req.params;
+            validateMongodbId(id, res);
+            const Address = await AddressService.getAddressByPK({ _id: id });
+
+            return res.status(201).json({
+                success: true,
+                data: Address
+            });
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "can not get data",
+                errMessage: err.message
+            });
+        }
+    }
+
+    static getAllAddressOfAUser = async (req, res) => {
+        try {
+            const { userId } = req.users;
+            validateMongodbId(id, res);
+            const all = await AddressService.getAUserAllAddressByUserId(userId);
+
+            return res.status(201).json({
+                success: true,
+                data: all
+            });
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                message: "can not get data",
+                errMessage: err.message
+            });
+        }
+    }
 }
 
 module.exports = AddressController;
--- routes/address.route.js
+++ routes/address.route.js
@@ -3,9 +3,11 @@ const addressRoute = express.Router();
 const AddressController = require("../controller/address.controller");
 const { authMiddleware, authorizeRole } = require('../middleware/auth.middleware');
 
-// Public route
-
-
 // protected route
+addressRoute.post('/add-address', [authMiddleware], AddressController.createAddress);
+addressRoute.post('/update-address', [authMiddleware], AddressController.updateAddress);
+addressRoute.post('/delete-address', [authMiddleware], AddressController.deleteAddress);
+addressRoute.post('/get-address', [authMiddleware], AddressController.getAddressById);
+addressRoute.post('/get-all-address', [authMiddleware], AddressController.getAllAddressOfAUser);
 
 module.exports = addressRoute;
